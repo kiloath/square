07d440cbf108cfcf5c8cb7dd319d43c0ce5d0e3c
Peter <peter.hsu@concords.com.tw>
2024-07-26 11:15:32 +0800

    ,

---
+++ workspace/korust/korust_build/.devcontainer/rebuild_korust.ps1
@@ -0,0 +1,16 @@
+$korust = 'korust'
+$w='~\korice\korust_build'
+cd ~;rm $w -r -fo;ni $w -i d -f;cd $w
+copy -r "~\korice\square\workspace\$korust\korust_build\.devcontainer" .
+cd .\.devcontainer\
+copy "~\korice\square\assets\export_crt.ps1" .
+.\export_crt.ps1
+docker rm $korust -f
+docker rmi "${korust}base"
+docker rmi $korust
+docker build -t "${korust}base:latest" .
+devcontainer build --workspace-folder .. --image-name "${korust}:latest"
+cd ~;rm $w -r -fo
+$w="~\korice\$korust"
+cd ~;rm $w -r -fo;ni $w -i d -f;cd $w
+copy -r "~\korice\square\workspace\$korust\korust\.devcontainer" . -fo
