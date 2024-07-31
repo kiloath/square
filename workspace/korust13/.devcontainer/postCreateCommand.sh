be72526c85d9be0e262c4d23038877390036e802
Peter <peter.hsu@concords.com.tw>
2024-07-31 15:42:45 +0800

    ,

--- workspace/korust13/.devcontainer/postCreateCommand.sh
+++ workspace/korust13/.devcontainer/postCreateCommand.sh
@@ -1,10 +1,2 @@
 git config --global user.name Kiloath
 git config --global user.email Kiloath@example.com
-cat >  /etc/docker/daemon.json <<EOF
-{
-  "features": {
-    "containerd-snapshotter": false
-  },
-  "insecure-registries" : [ "192.168.103.107:5000" ]
-}
-EOF
