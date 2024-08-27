bf0177b7dbf6a88ee4fa0c574c8ba7c80800b7b8
Peter <peter.hsu@concords.com.tw>
2024-08-27 12:24:16 +0800

    ,

---
+++ korust01/korust01_00.md
@@ -0,0 +1,7 @@
+# korust01_00
+* 只提供 latest
+```
+~\korice\square\workspace\korust\korust_build\.devcontainer\rebuild_korust.ps1
+docker login
+docker push kiloath\korust
+```
--- workspace/korust/korust_build/.devcontainer/rebuild_korust.ps1
+++ workspace/korust/korust_build/.devcontainer/rebuild_korust.ps1
@@ -8,8 +8,8 @@ cd .\.devcontainer\
 copy "~\korice\square\assets\export_crt.ps1" .
 .\export_crt.ps1
 docker rmi "${korust}base"
-docker rmi "kiloath\$korust"
+docker rmi "kiloath/$korust"
 docker build -t "${korust}base:latest" .
-devcontainer build --workspace-folder .. --image-name "kiloath\${korust}:latest"
+devcontainer build --workspace-folder .. --image-name "kiloath/${korust}:latest"
 docker rmi "${korust}base"
 cd ~;rm $w -r -fo
