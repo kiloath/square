01f3c2b2200036906d26695fda481db72b196aeb
Peter <peter.hsu@concords.com.tw>
2024-12-11 11:21:02 +0800

    ,

--- images/korust1/rebuild_korust.ps1
+++ images/korust1/rebuild_korust.ps1
@@ -2,12 +2,12 @@ $korust = 'korust1'
 $version = '1.0'
 $w='~\korice\korust_build'
 cd ~
+rm $w -r -fo
 if(Test-Path $w) {rm $w -r -fo}
 ni $w -i d -f;cd $w
-copy -r "~\korice\square\images\$korust\script" .
-cd .\.devcontainer\
+copy "~\korice\square\images\$korust\script\*" .
 copy "~\korice\square\assets\export_crt.ps1" .
 & .\export_crt.ps1
 docker rmi $korust
-docker build -t "${korust}:latest" .
+docker build -t "${korust}:${version}" .
 cd ~;rm $w -r -fo
